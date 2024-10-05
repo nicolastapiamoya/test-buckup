edc93308f8b0edc2b11d63a8b751099dfac0452b
nicolastapiamoya <nicousm46@gmail.com>
2024-10-05 02:02:43 -0300

    update build

--- src/app/api/auth/user/route.js
+++ src/app/api/auth/user/route.js
@@ -19,6 +19,23 @@ export async function login(credentials) {
     }
 }
 
+export async function POST(req) {
+    const body = await req.json();
+    try {
+        const res = await fetch(`${configHost.host}/shop-user`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+        const data = await res.json();
+        return new Response(JSON.stringify(data), { status: 'ok' });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Error adding category' }), { status: 500 });
+    }
+}
+
 export async function addUser(body) {
     try {
         const res = await fetch(`${configHost.host}/shop-user`, {
@@ -52,6 +69,26 @@ export async function allUsers() {
         return data
     } catch (error) {
         return error
+    }
+}
+
+export async function GET() {
+    try {
+        const res = await fetch(`${configHost.host}/shop-user`);
+        const data = await res.json();
+        return new Response(JSON.stringify(data), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Error fetching users' }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
     }
 }
 
